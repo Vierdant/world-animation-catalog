b1dfa6727a7a4146afef43866c10f011526556a8
vierdant <vierdant@gmail.com>
2025-07-26 18:51:42 +0200

    more stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,8 +13,36 @@
   };
 
   const tagColors = {
-    female: '#ffb6c1',
-    male: '#add8e6'
+    female: '#EB6E7D',
+    male: '#3184A0',
+    scene: '#242042',
+    loop: '#5C4622',
+    pose: '#33665F',
+    sitting: '#212121',
+    laying: '#212121',
+    standing: '#212121',
+    food: '#212121',
+    working: '#212121',
+    expressions: '#212121',
+    gangsigns: '#212121',
+    phone: '#212121',
+    ground: '#212121',
+    walking: '#212121',
+    smoking: '#212121',
+    leaning: '#212121',
+    surrender: '#212121',
+    social: '#212121',
+    regular_dances: '#212121',
+    rave_dances: '#212121',
+    professional_dances: '#212121',
+    adult: '#BD1E1E',
+    items: '#212121',
+    gestures: '#212121',
+    fighting: '#212121',
+    exercises: '#212121',
+    poses: '#212121',
+    tactical: '#212121',
+    vehicle: '#212121',
   }
 
 
